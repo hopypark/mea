2f3cd149166a3ea49924f6a6045292541e4cc7aa
hopypark <hopypark@gmail.com>
2022-10-27 18:01:01 +0900

    modified to be applicable mulit-selection

--- calculateMedUsingYenikimFnn.m
+++ calculateMedUsingYenikimFnn.m
@@ -7,41 +7,62 @@
 clc
 clearvars
 
-%
-max_dimension = 10;
-tau=2;
-rtol = 10;
-atol = 2;
-%
-    %% 1s block
-    nRows = 600; % number of blocks when time is 1 sec
-    nData = 12500; % number of data when time is 1 sec
-    nCols = 8;
-    %
-    shiftdata = 25;
-    shiftblock = 30;
+% variables about f_fnn
+%%
+MAXIMUM_DIM = 10;
+TIME_DELAY = 2;
+R_THRESHOLD = 10;
+A_THRESHOLD = 2;
+% constant variable
+max_dimension = MAXIMUM_DIM;
+tau=TIME_DELAY;
+rtol = R_THRESHOLD;
+atol = A_THRESHOLD;
 
-    numTotalShiftBlock = 20;
-    numCurrShiftBlock = 0;
-    numWells = 96;
+% variables about electrode data
+%% 1s block
+SAMPLING_FREQ = 12500; % fs
+SHIFT_DATA_LENGTH = 25; % 12.5kHz --> 500Hz down sampling
+BLOCK_LENGTH = 30;
+SAMPLE_NUMBER_SECOND = SAMPLING_FREQ;
+NUMBER_WELLS = 96;
+NUMBER_ELECTRODES = 8;
 
-electsNames = {'electrode11';'electrode21';'electrode12';'electrode13';'electrode22';'electrode23';'electrode31';'electrode33'};
+nRows = 0; % number of blocks when time is 1 sec
+nData = SAMPLE_NUMBER_SECOND; % number of data when sampling time is 1 sec
+nCols = NUMBER_ELECTRODES;
+%
+shiftdata = SHIFT_DATA_LENGTH;
+shiftblock = BLOCK_LENGTH;
+
+numTotalShiftBlock = 0;%20; nRows / shift_block_length
+numCurrShiftBlock = 0;
+numWells = NUMBER_WELLS;
+
+%
+
+electsNames = {'electrode11';'electrode12';'electrode13';'electrode21';'electrode22';'electrode23';'electrode31';'electrode33'};
+numElects = size(electsNames,1);
 wellsname = '';
-
-fnn1=zeros(numTotalShiftBlock * numWells, nCols);
-%fnn2=zeros(nRows/shiftblock, nCols);
-tmpnames=cell(numTotalShiftBlock * numWells,1);
 
 [files,path] = uigetfile({'*.mat'}, 'Select One or More Files', 'D:\002.matlab\yenikim\data\','MultiSelect','On');
 [r, filesLen] = size(files); % # number of selected files
 disp([num2str(filesLen) ' files selected.'])
+
+%
+% global variables for FNN and wellnames
+% - dynamic array
+fnn=[];%zeros(numTotalShiftBlock * numWells, nCols);
+wellnames=[];%cell(numTotalShiftBlock * numWells,1);
+
 tic
 for ifile = 1:filesLen
 
     elects = load([path files{1,ifile}],'Electrodes');
-    wellsname = files{1,ifile}(1,24:26); % A01, A02, ..., A12, B01, ..., B12, ... , H12
-%     elects = load([path files],'Electrodes');
-%     wellsname = files(1,24:26);
+
+    wname = files{1,ifile}(1,24:26); % A01, A02, ..., A12, B01, ..., B12, ... , H12
+    %     elects = load([path files],'Electrodes');
+    %     wellsname = files(1,24:26);
 
     enames = fieldnames(elects.Electrodes);
     if ~compareCellStrings(electsNames, enames)
@@ -49,8 +70,6 @@ for ifile = 1:filesLen
         break;
     end
 
-
-    numElects = size(electsNames);
     %
     % % 0.2s block
     % nRows = 3000; % number of blocks when time is 0.2 sec
@@ -59,7 +78,7 @@ for ifile = 1:filesLen
     %
     % fnn1=zeros(nRows, nCols);
     % fnn2=zeros(nRows, nCols);
-    % 
+    %
     % for iCol = 1:numElects
     %     disp(['name: ' electsNames{iCol,1}])
     %     elecData = double(elects.Electrodes.(electsNames{iCol,1}).Data);
@@ -80,65 +99,70 @@ for ifile = 1:filesLen
     % end
     % toc
 
-%     %% 1s block
-%     nRows = 600; % number of blocks when time is 1 sec
-%     nData = 12500; % number of data when time is 1 sec
-%     nCols = 8;
-%     %
-%     shiftdata = 25;
-%     shiftblock = 30;
-% 
-%     numTotalShiftBlock = 20;
-%     numCurrShiftBlock = 0;
+    %     %% 1s block
+    %     nRows = 600; % number of blocks when time is 1 sec
+    %     nData = 12500; % number of data when time is 1 sec
+    %     nCols = 8;
+    %     %
+    %     shiftdata = 25;
+    %     shiftblock = 30;
+    %
+    %     numTotalShiftBlock = 20;
+    %     numCurrShiftBlock = 0;
 
-
-    displog = sprintf('%s - %02d/%02d..............%06.2f%% Completed.', files{1,ifile}, ifile, numWells , ifile/numWells*100);
+    displog = sprintf('%s - %02d/%02d..............%06.2f%% Completed.', files{1,ifile}, ifile, filesLen , ifile/filesLen*100);
     disp(displog)
+
+    nRows = size(elects.Electrodes.(electsNames{1,1}).Data, 1) / SAMPLE_NUMBER_SECOND; % return # of 1s block
+    numTotalShiftBlock = nRows/shiftblock; % 20(10 minutes data) or 30(15 minutes data )
+    %
+    % block unit variables
+    fnn1=zeros(numTotalShiftBlock, nCols);
+    wellname=cell(numTotalShiftBlock,1);
 
     for iCol = 1:numElects
         disp(['electrode name: ' electsNames{iCol,1}])
         elecData = double(elects.Electrodes.(electsNames{iCol,1}).Data);
-        
+        %         nRows = size(elecData, 1) / SAMPLE_NUMBER_SECOND; % return # of 1s block
         for iRow = 1:shiftblock:nRows % downsampling - 30초 shift => 600개 중 20개 계산
             numCurrShiftBlock = numCurrShiftBlock + 1;
             data=elecData((iRow - 1) * nData + 1 :shiftdata:iRow * nData, 1 );
             standard = (data - mean(data))./std(data); %// Standardization normalization
             [val, ed1] = min(f_fnn(standard, tau, max_dimension, rtol, atol));
-            idx = numTotalShiftBlock * (ifile - 1) + numCurrShiftBlock;
-            fnn1(idx, iCol) = ed1;
+            %idx = numTotalShiftBlock * (ifile - 1) + numCurrShiftBlock;
+            fnn1(numCurrShiftBlock, iCol) = ed1;
             %         [fnnPerc, embTimes] = mdFnn(standard, tau);
             %         [val, ed2] = min(fnnPerc);
             %         fnn2(iRow, iCol) = ed2;
             % displog = sprintf('%03d/%04d..............%06.2f%% Completed.', iRow, nRows, iRow/nRows*100);
             %         displog = sprintf('%03d/%04d..............%05.2f%% Completed(f_fNN = %2d, mdFnn = %2d)', iRow, nRows, iRow/nRows*100, ed1, ed2);
-%             displog = sprintf('[%04d] %02d/%02d..............%06.2f%% Completed(f_fNN = %2d)', idx,numCurrShiftBlock, numTotalShiftBlock, numCurrShiftBlock/numTotalShiftBlock*100, ed1);
-%             disp(displog)
-            tmpnames{idx, 1} = sprintf([wellsname '-%02d'],numCurrShiftBlock);
-        end 
+            %             displog = sprintf('[%04d] %02d/%02d..............%06.2f%% Completed(f_fNN = %2d)', idx,numCurrShiftBlock, numTotalShiftBlock, numCurrShiftBlock/numTotalShiftBlock*100, ed1);
+            %             disp(displog)
+            wellname{numCurrShiftBlock, 1} = sprintf([wname '-%02d'],numCurrShiftBlock);
+        end
         numCurrShiftBlock = 0;
-        
+
     end % electrodes
-    % wells name
-%     for i=1:20
-%         tmpnames{iCol * numTotalShiftBlock + i, 1} = sprintf([wellsname '-%02d'],i);
-%     end
+
+    % add to global variable
+    fnn = [fnn; fnn1]; fnn1=[];
+    wellnames = [wellnames; wellname]; wellname=[];
 
 end
 
+%
+% make a table using wellnames and f_fnn results
+%
 tableValue = table;
 
-tableValue.wellname = tmpnames;
-tableValue.electrode11 = fnn1(:,1);
-tableValue.electrode21 = fnn1(:,2);
-tableValue.electrode12 = fnn1(:,3);
-tableValue.electrode13 = fnn1(:,4);
-tableValue.electrode22 = fnn1(:,5);
-tableValue.electrode23 = fnn1(:,6);
-tableValue.electrode31 = fnn1(:,7);
-tableValue.electrode33 = fnn1(:,8);
+tableValue.wellname = wellnames;
+for i=1:numElects
+    eval(sprintf('tableValue.%s = fnn(:,%d);',electsNames{i,1},i));
+end
 
 % write a table to csv file
-writetable(tableValue,[path files{1,1}(1,1:22) '.f_fnn.csv'],'Delimiter',',','QuoteStrings',false)
+%writetable(tableValue,[path files{1,1}(1,1:22) '.f_fnn.csv'],'Delimiter',',','QuoteStrings',false)
+writetable(tableValue,[strrep(path,'mat\','') files{1,1}(1,1:22) '.f_fnn.csv'],'Delimiter',',','QuoteStrings',false)
 toc
 
 
--- loadPortionAxISRawData.m
+++ loadPortionAxISRawData.m
@@ -2,12 +2,16 @@
 % created by KT Park
 % date: 2022.09.08
 %
-% date: 2022.10.25 ~ 
-% - 다양한 파일에 적용 가능하도록 수정 
-%
+% date: 2022.10.25 ~ 26
+% - modified to be applicable to various file(.raw) size.
+% 
 clear;
 
-[file, path] = uigetfile({'*.raw';'*.*'},'Select a File(s)','D:\002.matlab\yenikim\data');
+[files, path] = uigetfile({'*.raw';'*.*'},'Select One or More Files','D:\002.matlab\yenikim\data','MultiSelect','Off');
+
+[r, filesLen] = size(files); % # number of selected files
+disp([num2str(filesLen) ' files selected.'])
+
 %disp([path file])
 disp(['Change directory to ' path])
 cd(path)
@@ -25,116 +29,125 @@ numWellRows = 8;
 % - Well Column(12): 1, 2, ... ,12
 numWellCols = 12;
 
-% - one signal per electrode
-disp(['Loading...' file])
+
+% for ifile = 1:filesLen
 tic
-AllData = AxisFile([path file]).DataSets.LoadData;
-toc
-disp('Loading Complete')
-% %%
-% well.info=[];
-% for wrows = 1:numWellRows
-%     for wcols = 1:numWellCols
-%         %well.WellName = ROW_NAME{}
-%                 % (col=1, row=1)
-%                 electrode11.Row = 1;
-%                 electrode11.Column = 1;
-%                 electrode11.Data = AllData{wrows, wcols, 1, 1}.Data;                
-%                 % (col=1, row=2)
-%                 electrode12.Row = 2;
-%                 electrode12.Column = 1;
-%                 electrode12.Data = AllData{wrows, wcols, 1, 2}.Data;         
-%                 % (col=1, row=3)
-%                 electrode13.Row = 3;
-%                 electrode13.Column = 1;
-%                 electrode13.Data = AllData{wrows, wcols, 1, 3}.Data;     
-%                 %
-%                 % (col=2, row=1)
-%                 electrode21.Row = 1;
-%                 electrode21.Column = 2;
-%                 electrode21.Data = AllData{wrows, wcols, 2, 1}.Data;  
-%                 % (col=2, row=2)
-%                 electrode22.Row = 2;
-%                 electrode22.Column = 2;
-%                 electrode22.Data = AllData{wrows, wcols, 2, 2}.Data;
-%                 % (col=2, row=3)
-%                 electrode23.Row = 3;
-%                 electrode23.Column = 2;
-%                 electrode23.Data = AllData{wrows, wcols, 2, 3}.Data;                
-%                 %
-%                 % (col=3, row=1)
-%                 electrode31.Row = 1;
-%                 electrode31.Column = 3;
-%                 electrode31.Data = AllData{wrows, wcols, 3, 1}.Data;  
-% %                 % (col=3, row=2)
-% %                 electrode32.Row = 2;
-% %                 electrode32.Column = 3;
-% %                 electrode32.Data = AllData{wrows, wcols, 3, 2}.Data;
-%                 % (col=3, row=3)
-%                 electrode33.Row = 3;
-%                 electrode33.Column = 3;
-%                 electrode33.Data = AllData{wrows, wcols, 3, 3}.Data;                 
-% 
-%                 well.Electrodes.electrode11 = electrode11;
-%                 well.Electrodes.electrode12 = electrode12;
-%                 well.Electrodes.electrode13 = electrode13;
-% 
-%                 well.Electrodes.electrode21 = electrode21;
-%                 well.Electrodes.electrode22 = electrode22;
-%                 well.Electrodes.electrode23 = electrode23;
-% 
-%                 well.Electrodes.electrode31 = electrode31;
-% % blank         well.Electrodes.electrode32 = electrode32;
-%                 well.Electrodes.electrode33 = electrode33;
-% 
-%                 wellname=[ROW_NAME{1, AllData{wrows, wcols, 3, 3}.Channel.WellRow} sprintf('%02d',AllData{wrows, wcols, 3, 3}.Channel.WellColumn)];
-% 
-%         well.WellName = wellname;
-%         tmpname = strrep(file, '.raw', ['.' wellname '.mat']);
-%         save(tmpname,'-struct', 'well');
-%     end
-% end
-% 
-% % 2022.09.16 completed
+    % - one signal per electrode
+    disp(['Loading...' files])
 
+%    AllData = AxisFile([path files{1,ifile}]).DataSets.LoadData;
+    AllData = AxisFile([path files]).DataSets.LoadData;
+    disp('Loading Complete')
 
-% ////////////////////////////////
-% type check. 2022.10.25~26
-%
-% well data extraction using cell's type check(empty cell)
-% ////////////////////////////////
-well.info=[];
-for wrows = 1:numWellRows
-    for wcols = 1:numWellCols
-        % extract electrodes data for a well 
-        for ecols = 1:3
-            for erows = 1:3
-                try
-                    e=AllData{wrows, wcols, ecols,erows}.Data;
-                    %sprintf('%d, %d array is waveform.', ecols , erows)
-                     %str = 'sprintf("electrode%d%d.row=AllData{%d, %d, %d, %d}.Data;",ecols,erows,wrows,wcols,ecols,erows)';
-                     eval(sprintf("well.Electrodes.electrode%d%d.Row= %d;",ecols,erows, erows));
-                     eval(sprintf("well.Electrodes.electrode%d%d.Column=%d;",ecols,erows,ecols));
-                     eval(sprintf("well.Electrodes.electrode%d%d.Data=AllData{%d, %d, %d, %d}.Data;",ecols,erows,wrows,wcols,ecols,erows));
-                catch
-                    msg=sprintf('%d, %d array is empty.', ecols , erows);
-                    disp(msg)
-                    continue;
+%     [filepath, name, ext] = fileparts(files{1,ifile});
+    [filepath, name, ext] = fileparts(files);
+    % %%
+    % well.info=[];
+    % for wrows = 1:numWellRows
+    %     for wcols = 1:numWellCols
+    %         %well.WellName = ROW_NAME{}
+    %                 % (col=1, row=1)
+    %                 electrode11.Row = 1;
+    %                 electrode11.Column = 1;
+    %                 electrode11.Data = AllData{wrows, wcols, 1, 1}.Data;                
+    %                 % (col=1, row=2)
+    %                 electrode12.Row = 2;
+    %                 electrode12.Column = 1;
+    %                 electrode12.Data = AllData{wrows, wcols, 1, 2}.Data;         
+    %                 % (col=1, row=3)
+    %                 electrode13.Row = 3;
+    %                 electrode13.Column = 1;
+    %                 electrode13.Data = AllData{wrows, wcols, 1, 3}.Data;     
+    %                 %
+    %                 % (col=2, row=1)
+    %                 electrode21.Row = 1;
+    %                 electrode21.Column = 2;
+    %                 electrode21.Data = AllData{wrows, wcols, 2, 1}.Data;  
+    %                 % (col=2, row=2)
+    %                 electrode22.Row = 2;
+    %                 electrode22.Column = 2;
+    %                 electrode22.Data = AllData{wrows, wcols, 2, 2}.Data;
+    %                 % (col=2, row=3)
+    %                 electrode23.Row = 3;
+    %                 electrode23.Column = 2;
+    %                 electrode23.Data = AllData{wrows, wcols, 2, 3}.Data;                
+    %                 %
+    %                 % (col=3, row=1)
+    %                 electrode31.Row = 1;
+    %                 electrode31.Column = 3;
+    %                 electrode31.Data = AllData{wrows, wcols, 3, 1}.Data;  
+    % %                 % (col=3, row=2)
+    % %                 electrode32.Row = 2;
+    % %                 electrode32.Column = 3;
+    % %                 electrode32.Data = AllData{wrows, wcols, 3, 2}.Data;
+    %                 % (col=3, row=3)
+    %                 electrode33.Row = 3;
+    %                 electrode33.Column = 3;
+    %                 electrode33.Data = AllData{wrows, wcols, 3, 3}.Data;                 
+    % 
+    %                 well.Electrodes.electrode11 = electrode11;
+    %                 well.Electrodes.electrode12 = electrode12;
+    %                 well.Electrodes.electrode13 = electrode13;
+    % 
+    %                 well.Electrodes.electrode21 = electrode21;
+    %                 well.Electrodes.electrode22 = electrode22;
+    %                 well.Electrodes.electrode23 = electrode23;
+    % 
+    %                 well.Electrodes.electrode31 = electrode31;
+    % % blank         well.Electrodes.electrode32 = electrode32;
+    %                 well.Electrodes.electrode33 = electrode33;
+    % 
+    %                 wellname=[ROW_NAME{1, AllData{wrows, wcols, 3, 3}.Channel.WellRow} sprintf('%02d',AllData{wrows, wcols, 3, 3}.Channel.WellColumn)];
+    % 
+    %         well.WellName = wellname;
+    %         tmpname = strrep(file, '.raw', ['.' wellname '.mat']);
+    %         save(tmpname,'-struct', 'well');
+    %     end
+    % end
+    % 
+    % % 2022.09.16 completed
+    
+    
+    % ////////////////////////////////
+    % type check of Wells & Electrodes . 2022.10.25~26
+    %
+    % well data extraction using cell's type check(empty cell)
+    % ////////////////////////////////
+    well.info=[];
+    for wrows = 1:numWellRows
+        for wcols = 1:numWellCols
+            % extract electrodes data for a well 
+            for ecols = 1:3
+                for erows = 1:3
+                    try
+                        e=AllData{wrows, wcols, ecols,erows}.Data;
+                        %sprintf('%d, %d array is waveform.', ecols , erows)
+                         %str = 'sprintf("electrode%d%d.row=AllData{%d, %d, %d, %d}.Data;",ecols,erows,wrows,wcols,ecols,erows)';
+                         eval(sprintf("well.Electrodes.electrode%d%d.Row= %d;",ecols,erows, erows));
+                         eval(sprintf("well.Electrodes.electrode%d%d.Column=%d;",ecols,erows,ecols));
+                         eval(sprintf("well.Electrodes.electrode%d%d.Data=AllData{%d, %d, %d, %d}.Data;",ecols,erows,wrows,wcols,ecols,erows));
+                    catch
+                        msg=sprintf('%d, %d array is empty.', ecols , erows);
+                        disp(msg)
+                        continue;
+                    end
                 end
+            end % end of a well
+            wellname=[ROW_NAME{1, AllData{wrows, wcols, 3, 3}.Channel.WellRow} sprintf('%02d',AllData{wrows, wcols, 3, 3}.Channel.WellColumn)];
+            well.WellName = wellname;
+            tmpname = [name '.' wellname '.mat'];
+            % if 'mat' subfolder is not exist, then make subfolder that name is 'mat'
+            if not(exist(name, 'dir'))
+                mkdir([name '\mat'])
             end
-        end % end of a well
-        wellname=[ROW_NAME{1, AllData{wrows, wcols, 3, 3}.Channel.WellRow} sprintf('%02d',AllData{wrows, wcols, 3, 3}.Channel.WellColumn)];
-        well.WellName = wellname;
-        tmpname = strrep(file, '.raw', ['.' wellname '.mat']);
-        % if 'mat' subfolder is not exist, then make subfolder that name is 'mat'
-        if not(exist(tmpname, 'dir'))
-            mkdir([tmpname '\mat'])
+            %
+            % save a well data(data of 8-electrode) to .mat file 
+            save([name '\mat\' tmpname],'-struct', 'well');        
         end
-        %
-        % save a well data(data of 8-electrode) to .mat file 
-        save(['mat\' tmpname],'-struct', 'well');        
     end
-end
-% end
+    % end
 
-
+%     emsg=sprintf('%03d/%03d files completed.\n',ifile,  num2str(filesLen));
+%     disp(emsg)
+% end % for files
+toc
